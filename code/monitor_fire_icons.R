7c0864cff074804a811c6e8addb1ac4e8c327cce
jeremyash <jeremyd.ash@gmail.com>
2025-04-03 10:57:09 -0400

    updated path for pngs and added comments for code

--- code/monitor_fire_icons.R
+++ code/monitor_fire_icons.R
@@ -1,17 +1,19 @@
-# create points to be used in legend for smokeR
+# create points to be used in legend for smokeR and other reporting tools, modeled after Fire and Smoke Map icons
 
-#  
-png("smokeR/perm_mon.png", width = 900, height = 900, res = 10500, bg = "transparent")
+#  create icon for permanent monitors
+png("perm_mon.png", width = 900, height = 900, res = 10500, bg = "transparent")
 par(mar = c(0, 0, 0, 0))
 plot.new()
 points(.5, .5, pch = 21, col = "black", bg = "#00E400")
 dev.off()
 
-
-png("smokeR/temp_mon.png", width = 900, height = 900, res = 10500, bg = "transparent")
+#  create icon for temp monitors
+png("temp_mon.png", width = 900, height = 900, res = 10500, bg = "transparent")
 par(mar = c(0, 0, 0, 0))
 plot.new()
 points(.5, .5, pch = 21, cex = 0.5, lwd = 0.5,  col = "black", bg = "#00E400")
 dev.off()
+
+
   
 
